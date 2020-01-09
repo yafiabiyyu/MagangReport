643c6d78de6c0f861052f9540095760ef3fb9d2e
yafiabiyyu <luckyveri3@gmail.com>
2020-01-09 09:40:21 +0700

    update login

--- src/auth/auth_login.php
+++ src/auth/auth_login.php
@@ -1,18 +1,19 @@
 <?php
 session_start();
-
 require_once('../../config.php');
-$username = $_POST['username'];
-$password = $_POST['password'];
-$login_query = "SELECT * FROM staff where username = '".$username."' and password = '".$password."'";
-$sql_result = mysqli_query($host,$login_query);
-
-$cek = mysqli_num_rows($sql_result);
-if ($cek > 0) {
-    echo "<script>alert('Selamat Datang');
-    window.location='../home/dashboard.php'</script>";
-}else {
-    echo "<script>alert('Selamat Datang');
-    window.location='../../index.html'</script>";
+if (isset($_POST['submit'])) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $login_query = "SELECT * FROM staff where username = '".$username."' and password = '".$password."'";
+    $sql_result = mysqli_query($host,$login_query);
+    $cek = mysqli_num_rows($sql_result);
+    if ($cek > 0) {
+        $_SESSION['username'] = $username;
+        echo "<script>alert('Selamat Datang');
+        window.location='../home/dashboard.php'</script>";
+    }else {
+        echo "<script>alert('Gagal Login');
+        window.location='../../index.html'</script>";
+    }
 }
 ?>
