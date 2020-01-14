17db80ab53ef30eddd157944f8ed4748c15b98b7
yafiabiyyu <luckyveri3@gmail.com>
2020-01-14 14:45:03 +0700

    update tmp datatables

--- js/demo/datatables-demo.js
+++ js/demo/datatables-demo.js
@@ -1,7 +1,61 @@
-// Call the dataTables jQuery plugin
-$(document).ready(function() {
-  $('#dataTable').DataTable({
-    paging: true,
-    searching: false
+$(document).ready(function () {
+  var table = $('#dataTable').DataTable();
+
+  // Event listener to the two range filtering inputs to redraw on input
+  $('#dateFrom, #dateTo').change(function () {
+      table.draw();
   });
 });
+
+// $.fn.dataTable.ext.search.push(
+//   function (settings, data, dataIndex) {
+//       var dateFrom = moment(document.getElementById("dateFrom").value).format('MM/DD/YYYY');
+//       var dateTo = moment(document.getElementById("dateTo").value).format('MM/DD/YYYY');
+//       var startDate = data[3];
+//       if (dateFrom == null && dateTo == null) {return false;}
+//       if (dateFrom == null && startDate <= dateTo) {return true;}
+//       if (dateTo == null && startDate >= dateFrom) {return true;}
+//       if (startDate <= dateTo && startDate >= dateFrom) {return true;}
+//       return false;
+//   }
+// );
+
+// $.fn.dataTableExt.afnFiltering.push(
+//   function( settings, data, dataIndex ) {
+//       var min  = $('#dateFrom').val();
+//       var max  = $('#dateTo').val();
+//       var createdAt = data[3] || 0; // Our date column in the table
+//       //createdAt=createdAt.split(" ");
+//       var startDate   = new Date(min).();
+//       var endDate     = moment(max).format("DD/MM/YYYY");
+//       var diffDate = moment(createdAt).format("DD/MM/YYYY");
+//       console.log(startDate);
+//       if (
+//         (min == "" || max == "") ||
+//         (diffDate.isBetween(startDate, endDate,'days'))
+
+
+//       ) {  return true;  }
+//       return false;
+
+//   }
+// );
+
+$.fn.dataTable.ext.search.push(
+  function( settings, data, dataIndex ) {
+      var min  = $('#dateFrom').val();
+      var max  = $('#dateTo').val();
+      var createdAt = data[3] || 0; // Our date column in the table
+      console.log();
+      
+      if  ( 
+              ( min == "" || max == "" )
+              || 
+              ( moment(createdAt).isSameOrAfter(min) && moment(createdAt).isSameOrBefore(max) ) 
+          )
+      {
+          return true;
+      }
+      return false;
+  }
+);
